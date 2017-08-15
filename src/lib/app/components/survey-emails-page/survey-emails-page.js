fb55ca3a049c2d5fd8817d5b42afa59086f2ab3b
Nick Collings <nick@collingo.com>
2017-08-15 14:25:49 +0100

    Make survey send page stateful not, update components

--- src/lib/app/components/survey-emails-page/survey-emails-page.js
+++ src/lib/app/components/survey-emails-page/survey-emails-page.js
@@ -7,39 +7,33 @@ const getStyle = require('./survey-emails-page.css')
 const PageHeader = require('../page-header/page-header')
 const { cssProcessor } = require('../../lib/css')
 
-module.exports = class SurveyEmailsPage extends React.Component {
-  constructor (props) {
-    super(props)
-    this.style = getStyle()
-  }
+module.exports = (props) => {
+  const style = getStyle()
+  const surveyMessage = get(props, 'surveyMessage')
+  const emailSubject = get(surveyMessage, 'subject')
+  const hirer = get(surveyMessage, 'hirer')
+  const recipients = get(surveyMessage, 'recipients', [])
+  const recipientsList = recipients.join(', ')
+  const body = get(surveyMessage, 'body')
 
-  render () {
-    const surveyMessage = get(this.props, 'surveyMessage')
-    const emailSubject = get(surveyMessage, 'subject')
-    const hirer = get(surveyMessage, 'hirer')
-    const recipients = get(surveyMessage, 'recipients', [])
-    const recipientsList = recipients.join(', ')
-    const body = get(surveyMessage, 'body')
-
-    return (
-      <div className={this.style.pageBody}>
-        <Helmet>
-          <title>{'nudj - survey email'}</title>
-        </Helmet>
-        <PageHeader title={'Survey Email'} subtitle={`${hirer}`} />
-        <div className={this.style.pageContent}>
-          <div className={this.style.pageMain}>
-            <EmailForm
-              css={cssProcessor}
-              renderMessage={message => message}
-              recipients={recipientsList}
-              subject={emailSubject}
-              template={body}
-            />
-          </div>
-          <div className={this.style.pageSidebar} />
+  return (
+    <div className={style.pageBody}>
+      <Helmet>
+        <title>nudj - survey email</title>
+      </Helmet>
+      <PageHeader title='Survey Email' subtitle={hirer} />
+      <div className={style.pageContent}>
+        <div className={style.pageMain}>
+          <EmailForm
+            css={cssProcessor}
+            renderMessage={message => message}
+            recipients={recipientsList}
+            subject={emailSubject}
+            template={body}
+          />
         </div>
+        <div className={style.pageSidebar} />
       </div>
-    )
-  }
+    </div>
+  )
 }
