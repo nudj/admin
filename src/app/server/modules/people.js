dfd7a43109125216501cf1e8dd29b2358a7b4281
Timothy Robinson <timmyrobby@gmail.com>
2018-01-31 11:25:08 +0000

    Display only hirers instead of all people

--- src/app/server/modules/people.js
+++ src/app/server/modules/people.js
@@ -1,5 +1,5 @@
 const request = require('@nudj/framework/request')
-const { LogThenNotFound } = require('@nudj/framework/errors')
+const { NotFound } = require('@nudj/framework/errors')
 const { promiseMap } = require('@nudj/library')
 
 function fetchPeople () {
@@ -9,7 +9,7 @@ function fetchPeople () {
 function fetchPerson (personId) {
   return request(`people/${personId}`)
     .catch(error => {
-      throw new LogThenNotFound('Person not found by id', personId, error)
+      throw new NotFound('Person not found by id', personId, error)
     })
 }
 
