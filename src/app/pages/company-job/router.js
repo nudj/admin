a3765e018ff1fc4bb1803be838648e97f265c1ec
Timothy Robinson <timmyrobby@gmail.com>
2018-03-26 14:28:14 +0100

    Update company-job `put` fetcher to GQL

--- src/app/pages/company-job/router.js
+++ src/app/pages/company-job/router.js
@@ -11,7 +11,7 @@ const Router = ({
   router.use(ensureLoggedIn)
 
   router.getHandlers('/companies/:companySlug/jobs/:jobSlug', respondWithGql(fetchers.get))
-  router.putHandlers('/companies/:companySlug/jobs/:jobSlug', respondWith(fetchers.put))
+  router.putHandlers('/companies/:companySlug/jobs/:jobSlug', respondWithGql(fetchers.put))
   router.postHandlers('/companies/:companySlug/jobs/:jobSlug/referrals', respondWith(fetchers.postReferral))
   router.postHandlers('/companies/:companySlug/jobs/:jobSlug/referrals/:personId', respondWith(fetchers.postReferralPerson))
 
