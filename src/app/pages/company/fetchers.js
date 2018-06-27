f81a90c80134263c37b677db869697a86d62c22c
Nick Collings <nick@collingo.com>
2018-06-27 17:34:59 +0100

    major: Update person-form and people/person pages to support person and hirer manipulation

--- src/app/pages/company/fetchers.js
+++ src/app/pages/company/fetchers.js
@@ -218,9 +218,16 @@ function postHirer ({
   body
 }) {
   const gql = `
-    mutation GetCompanyPage ($slug: String!, $hirerData: HirerCreateByEmailInput!) {
+    mutation GetCompanyPage (
+      $slug: String!
+      $hirerEmail: String!
+      $hirerData: HirerCreateInput!
+    ) {
       company: companyByFilters(filters: { slug: $slug }) {
-        newHirer: createHirerByEmail(hirer: $hirerData) {
+        newHirer: createHirerByEmail(
+          email: $hirerEmail
+          hirer: $hirerData
+        ) {
           id
         }
         id
@@ -270,9 +277,14 @@ function postHirer ({
       }
     }
   `
+  const {
+    email: hirerEmail,
+    ...hirerData
+  } = body
   const variables = {
     slug: params.companySlug,
-    hirerData: body
+    hirerEmail,
+    hirerData
   }
 
   return { gql, variables }
