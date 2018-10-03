c9964a5f4f0367c78478b5d3cf1b9ad71d6829c3
Nick Collings <nick@collingo.com>
2018-10-03 17:24:15 +0100

    patch: Update api calls to pull details from env

--- src/webpack.config.client.js
+++ src/webpack.config.client.js
@@ -48,7 +48,7 @@ const config = {
         test: /\.js$/,
         include: [
           path.join(__dirname, 'app'),
-          path.join(__dirname, '@nudj'),
+          path.join(__dirname, 'yank', '@nudj'),
           path.join(__dirname, 'node_modules', '@nudj')
         ],
         exclude: /node_modules\/(?!@nudj)/,
@@ -93,7 +93,7 @@ if (process.env.USE_DEV_SERVER) {
   config.devServer = {
     contentBase: path.resolve(__dirname, 'app/server'),
     host: '0.0.0.0',
-    port: '83',
+    port: process.env.DEV_SERVER_PORT,
     publicPath: '/build/',
     public: process.env.DEV_SERVER_PATH,
     https: false,
